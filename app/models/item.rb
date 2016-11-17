b1645161654b231d458a4646618f3d56debd4c55
澤なつみ <n2natsu1218@gmail.com>
2016-11-17 21:30:50 +0900

    ・itemsテーブルのアソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ActiveRecord::Base
+  belongs_to :item_brands
+  belongs_to :item_categories
+  has_many :item_imgs
+  belongs_to :users
+  has_many :reviews
+  has_many :users, through: :reviews
+
 end
