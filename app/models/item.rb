3112d16ea1b22e86de59b981ab86ca39fd027427
澤なつみ <n2natsu1218@gmail.com>
2016-11-19 18:01:42 +0900

    ・items_categories周りのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,5 +5,7 @@ class Item < ActiveRecord::Base
   belongs_to :users
   has_many :reviews
   has_many :users, through: :reviews
+  has_many :item_categories
+  has_many :categories, through: :items_categories
 
 end
--- app/models/items_category.rb
+++ app/models/items_category.rb
@@ -1,2 +1,4 @@
 class ItemsCategory < ActiveRecord::Base
+  belongs_to :items
+  belongs_to :categories
 end
