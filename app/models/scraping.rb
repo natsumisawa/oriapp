ae185f38b475a50751512fa219dc201ecd1f1908
澤なつみ <n2natsu1218@gmail.com>
2016-11-19 16:38:31 +0900

    ・スクレイピングで各itemのcolorを抽出

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,25 +1,35 @@
 class Scraping
-  def self.get_links
-    links = []
-    agent = Mechanize.new
-    page = agent.get("http://www.cosme.net/item/item_id/802/products")
-    elements = page.search('.item-head .item a')
-    elements.each do |ele|
-      links << ele[:href]
-    end
+    def self.get_links
+        links = []
+        agent = Mechanize.new
+        page = agent.get("http://www.cosme.net/item/item_id/802/products")
+        elements = page.search('.item-head .item a')
+        elements.each do |ele|
+          links << ele[:href]
+        end
     #linksに商品の詳細ページURL情報が1ページ目の10件だけ入ってる
-    links.each do |link|
-      get_img(link)
+        links.each do |link|
+            get_img(link)
+            get_color(link)
+        end
     end
-  end
 
-  def self.get_img(link)#linkは引数
-    agent = Mechanize.new
-    page = agent.get(link)
-    imgs = page.search(".pic img")
-    imgs.each do |img|
-      puts img[:src]
+    def self.get_img(link)#linkは引数
+        agent = Mechanize.new
+        page = agent.get(link)
+        imgs = page.search(".pic img")
+        imgs.each do |img|
+          puts img[:src]
+        end
     end
+
+    def self.get_color(link)
+        agent = Mechanize.new
+        page = agent.get(link)
+        colors = page.search(".color-txt")
+        colors.each do |col|
+          puts col.inner_text
+        end
     end
 end
 
