d5a4837158fcf3669991e4d774be7ac724be3c7e
澤なつみ <n2natsu1218@gmail.com>
2016-11-20 17:35:51 +0900

    ・ユーザーの編集ページ作成途中 　・アクション作成まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])#URLを踏んだ時にユーザーのidがparamsで送られてくる、それを利用する
   end
+
+  def edit
+  end
+
+  def update
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show] #users/:idのようなURLになる
+  resources :users, only: [:show,:edit,:update] #users/:idのようなURLになる
   root to: "top#index"
 end
