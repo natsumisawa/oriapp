d77553d88e314544588331dacb7c434b03a1f907
澤なつみ <n2natsu1218@gmail.com>
2016-11-20 20:41:50 +0900

    ・まずはプロフィール設定をしてもらうようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ActiveRecord::Base
   validates_attachment_content_type :icon, content_type: ["image/jpg","image/jpeg","image/png"]
   has_many :reviews
   has_many :items,through: :reviews
+  def full_profile?
+    user_name? && icon?
+  end
 end
