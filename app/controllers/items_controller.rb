5d0d73e2d013a195c817ef82af3ef2c1708ba8ce
澤なつみ <n2natsu1218@gmail.com>
2016-11-21 02:46:09 +0900

    ・検索ページの表示でアソシエーション組んでるものが出てこない！

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   def search
     @items = Item.where('name LIKE(?)',"%#{params[:keyword]}%").limit(20)
+    @category_search_items = Item.find(25)
   end
 end
