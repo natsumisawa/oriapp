97d6ba88dfce5bfef4a5c6a45b849cfc649aa4e8
澤なつみ <n2natsu1218@gmail.com>
2016-11-21 19:01:35 +0900

    ・カテゴリ検索の表示奮闘

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,8 @@ class ItemsController < ApplicationController
     @search_items = Item.where('name LIKE(?)',"%#{params[:keyword]}%").limit(50)
     @items = Item.all
     @search_brand = ItemBrand.where('brand LIKE(?)',"%#{params[:keyword]}%").limit(50)
+    @categories = Category.all
+    binding.pry
+    @cat = Category.where(category: params[:category_input])
   end
-
-
 end
