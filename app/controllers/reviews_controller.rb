f916a0d9d5acd77d5a77302f201fefdb71e67fbb
澤なつみ <n2natsu1218@gmail.com>
2016-11-21 23:52:24 +0900

    ・review投稿機能の実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,18 @@ class ReviewsController < ApplicationController
   def new
     @items = Item.all
     @item_imgs = ItemImg.all
+    @review = Review.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    Review.create(review_params)
+    redirect_to user_path(current_user)
+  end
+
+private
+  def review_params
+    params.require(:review).permit(:how_often, :review, :item_img_id).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
