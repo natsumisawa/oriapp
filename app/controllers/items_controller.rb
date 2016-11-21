40904b6f3e499eba16e63793c1605ebe57920665
澤なつみ <n2natsu1218@gmail.com>
2016-11-21 15:37:28 +0900

    ・検索結果表示した ・ブランド名で検索

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
   def search
-    @items = Item.where('name LIKE(?)',"%#{params[:keyword]}%").limit(20)
-    @category_search_items = Item.find(25)
+    @search_items = Item.where('name LIKE(?)',"%#{params[:keyword]}%").limit(50)
+    @items = Item.all
+    @search_brand = ItemBrand.where('brand LIKE(?)',"%#{params[:keyword]}%").limit(50)
   end
+
+
 end
