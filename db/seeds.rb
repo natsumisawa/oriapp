12a9abaea9a0fb57fd31122b4f87711182f94caa
澤なつみ <n2natsu1218@gmail.com>
2016-11-25 02:14:54 +0900

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(email: 'tech-camp@gmail.com', password: '12345678', nickname: 'Mako')
+User.create(email: 'tech-camp@gmail.com', password: '12345678', user_name: 'Mako')
