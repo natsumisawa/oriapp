4db92d9449d48a967e6c0efea8c8c7f42dd87fce
澤なつみ <n2natsu1218@gmail.com>
2016-12-18 10:53:13 +0900

    ビューの変更

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
-    @imgs  = ItemImg.order('id ASC').limit(100)
+    @imgs  = ItemImg.find([1, 100, 500, 400])
     @users = User.all
   end
 end
